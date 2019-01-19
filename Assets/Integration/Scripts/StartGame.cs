bf4a25cb2a430d5551f32be9ca4cd75b001d36a8
PandoS0CE <paulopersovern@gmail.com>
2019-01-20 00:00:32 +0100

    integrateur

--- Assets/Integration/Scripts/StartGame.cs
+++ Assets/Integration/Scripts/StartGame.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class start : MonoBehaviour
+public class StartGame : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
