eefd658a429073784679e45d96fbe2b4eca24a8c
PandoS0CE <paulopersovern@gmail.com>
2019-01-20 00:20:55 +0100

    Ajout StartGame()

--- Assets/Integration/Scripts/StartGame.cs
+++ Assets/Integration/Scripts/StartGame.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class StartGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey("enter"))
+        {
+            GameController.instance.StartGame();
+        }
     }
 }
--- Assets/Integration/Scripts/controller.cs
+++ Assets/Integration/Scripts/controller.cs
@@ -83,6 +83,12 @@ public class Controller : MonoBehaviour
         }
 
     }
+
+    public void StartGame()
+    {
+        numLevel++;
+        ChangeLevel();
+    }
     // Start is called before the first frame update
     void Start()
     {
