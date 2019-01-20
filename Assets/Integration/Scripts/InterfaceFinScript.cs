97db451fc6959a3afafd22b0a28be209aab0358e
PandoS0CE <paulopersovern@gmail.com>
2019-01-20 12:25:31 +0100

    la fin mais avec un bug

---
+++ Assets/Integration/Scripts/InterfaceFinScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceFinScript : MonoBehaviour
+{
+    
+
+    public GameObject joueurBleu;
+    public GameObject joueurRed;
+
+    public GameObject textBleu;
+    public GameObject textRed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int globalScoreBlue = GlobalController.instance.GetGlobalScoreBlue();
+        int globalScoreRed = GlobalController.instance.GetGlobalScoreRed();
+
+        if (globalScoreBlue > globalScoreRed)
+        {
+            joueurBleu.SetActive(true);
+            joueurRed.SetActive(false);
+
+            textBleu.SetActive(true);
+            textRed.SetActive(false);
+        }
+        else
+        {
+            joueurBleu.SetActive(false);
+            joueurRed.SetActive(true);
+
+            textBleu.SetActive(false);
+            textRed.SetActive(true);
+        }
+
+}
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
