a91243367afb346d6284b8d7e6eb35ceba9aa932
PandoS0CE <paulopersovern@gmail.com>
2019-01-20 02:43:07 +0100

    anim sceneDebut

--- Assets/Integration/Scripts/StartGame.cs
+++ Assets/Integration/Scripts/StartGame.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 public class StartGame : MonoBehaviour
 {
     public GameObject textspace;
-    
+    public GameObject texttitle;
+    public float limitSize = 7.0f;
+
     // Start is called before the first frame update
     void Start()
     {
 
-        
-        
+        texttitle.transform.localScale = new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if((int)Time.time % 2 < 1)
+        int time = (int)Time.time;
+
+        if (texttitle.transform.localScale.x < limitSize)
+        {
+            texttitle.transform.localScale += new Vector3(0.1f, 0.1f, 0);
+        }
+
+
+        if (time % 2 < 1)
         {
             textspace.SetActive(true);
         }
         else
         {
-            textspace.SetActive(true);
+            textspace.SetActive(false);
         }
+
+        
 
         if(Input.GetKey(KeyCode.Space))
         {
